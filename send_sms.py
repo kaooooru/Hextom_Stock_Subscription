b4ad8b1fdbef71535b470d958216786fbcf6bc48
kaooooru <72216216+kaooooru@users.noreply.github.com>
2021-02-07 20:56:52 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACf790e37fc8dc05e01de60d1ca899fc0e"
+account_sid = "XXXX"
 # Your Auth Token from twilio.com/console
-auth_token  = "21991e47a869d8c814a5369901e12450"
+auth_token  = "XXXX"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+16478695209", 
+    to="+1XXXX", 
     from_="+12158762820",
     body="Hello from Python!")
 
