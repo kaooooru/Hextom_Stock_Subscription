e3a4bbce6da143b68c65fdd6dc725fa62e4f44ca
kaooooru <72216216+kaooooru@users.noreply.github.com>
2021-02-07 20:58:43 -0500

    Update views.py

--- mysite/stocksub/views.py
+++ mysite/stocksub/views.py
@@ -59,8 +59,8 @@ def stockPrice(ticker):
 
 
 def broadcast(request):
-    account_sid = "ACf790e37fc8dc05e01de60d1ca899fc0e"
-    auth_token  = "21991e47a869d8c814a5369901e12450"
+    account_sid = "XXXX"
+    auth_token  = "XXXX"
 
     client = Client(account_sid, auth_token)
 
